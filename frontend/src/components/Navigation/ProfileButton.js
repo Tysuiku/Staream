2f5c8d7862d936b93538d3b02ea36d04a4e9917f
Tysuiku <timothydong41@gmail.com>
2023-04-04 10:29:46 -0400

    fixedoff center issue with profile dropdown

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import * as sessionActions from "../../store/session";
 
@@ -6,54 +6,24 @@ const ProfileButton = ({ user }) => {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
 
-  const openMenu = (event) => {
-    event.stopPropagation(); // prevent the dropdown from closing when clicked
-    setShowMenu(true);
-  };
-
-  const closeMenu = () => {
-    setShowMenu(false);
-  };
-
-  useEffect(() => {
-    document.addEventListener("click", closeMenu);
-
-    return () => {
-      document.removeEventListener("click", closeMenu);
-    };
-  }, []);
-
   const logout = (event) => {
     event.preventDefault();
     dispatch(sessionActions.logout());
   };
 
   return (
-    <div className="profile-button">
-      <div>
-        <button id="profilebutton" onClick={openMenu}>
-          {`${user.username} ▾`}
-        </button>
-      </div>
+    <div className="profileButtonWrapper">
+      <button className="profileButton" onClick={() => setShowMenu(!showMenu)}>
+        {`${user.username} ▾`}
+      </button>
 
-      <div className="profileDropDownBox">
-        {showMenu && (
-          <ul
-            className="profile-dropdown"
-            onClick={(event) => event.stopPropagation()}
-          >
-            <li>
-              <button
-                id="profileLogoutButton"
-                onClick={logout}
-                className="logOutButton"
-              >
-                Logout: <span className="username">{user.username}</span>
-              </button>
-            </li>
-          </ul>
-        )}
-      </div>
+      {showMenu && (
+        <div className="profileButtonDropdown">
+          <button className="profileButtonDropdownItem" onClick={logout}>
+            Logout: <span className="logoutUserName">{user.username}</span>
+          </button>
+        </div>
+      )}
     </div>
   );
 };
